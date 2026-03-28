e0daf3b877bec85c1057a8faedf7c39136b70d4c
Raj1009-yad <rajnishkant7352@gmail.com>
2026-03-29 00:44:09 +0530

    Update courseController.js

--- lms-full-stack/server/controllers/courseController.js
+++ lms-full-stack/server/controllers/courseController.js
@@ -2,19 +2,21 @@ import Course from "../models/Course.js"
 
 
 // Get All Courses
+
 export const getAllCourse = async (req, res) => {
     try {
+
         const courses = await Course.find({ isPublished: true })
             .select(['-courseContent', '-enrolledStudents'])
-            .populate({ path: 'educator', select: '-password' });
+            .populate({ path: 'educator', select: '-password' })
 
-        res.json({ success: true, courses });
+        res.json({ success: true, courses })
 
     } catch (error) {
-        console.error("ERROR:", error); // 🔥 ADD THIS
-        res.status(500).json({ success: false, message: error.message });
+        res.json({ success: false, message: error.message })
     }
-};
+
+}
 // Get Course by Id
 export const getCourseId = async (req, res) => {
 
